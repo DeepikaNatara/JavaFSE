57f616ab335dd39b247d966c318909a420cf47df
DeepikaNatara <162548224+DeepikaNatara@users.noreply.github.com>
2024-03-07 12:48:01 +0530

    Update sumof2indexes.java

--- sumof2indexes.java
+++ sumof2indexes.java
@@ -1,3 +1,4 @@
+//int a[] = {10,-20,10,30,-10} sum of 2 indexes should be equal to -10 .. output = 4
 int a[] = {10,-20,10,30,-10};
 			int k=0;
 			for(int i=0,j=i+1,m=a.length-1,n=m-1;i<a.length-1;i++,j++,m--,n--)
